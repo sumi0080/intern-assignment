b2f0302285bc88431ba95b117bcc6c818c57fba8
Rasmus Friis <friis1978@gmail.com>
2021-09-20 20:23:24 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Head from 'next/head';
 
 export default function Home() {
-  const API_KEY = 'brug denne konstant til din api key, så vi nemt kan teste med vores egen senere'
+  const API_KEY = 'paste your movie apikey here so we can easily test your solution with our own apikey'
   
   return (
     <div>
       <Head>
-        <title>Intern assignment</title>
+        <title>Student assignment</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <p className="text-xl font-medium text-gray-800 mx-10 my-10">Movielist</p>
