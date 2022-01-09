d578782019a166f3af04c5ab7f712b2aab4729fb
Sumit <sumittimus8@gmail.com>
2022-01-09 21:16:08 +0100

    fetch api key

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,20 @@ import React from 'react';
 import Head from 'next/head';
 
 export default function Home() {
-  const API_KEY = 'paste your movie apikey here so we can easily test your solution with our own apikey'
+  const API_KEY = 'ee7f05f3fa5945387b2abca8d759fa0c'
+  const url = "https://api.themoviedb.org/3/movie/550?api_key=ee7f05f3fa5945387b2abca8d759fa0c"
+  
+  fetch(url, {
+    method:"GET"
+  })
+  .then(resp => resp.json())
+  .then(function(data)
+  {
+    console.log(data);
+  })
+  .catch(function(error){
+    console.log(error);
+  });
   
   return (
     <div>
